155d1f336ddd5b8c2e3205d967b10d6e0aed4143
Manuel <mninahuanca4@gmail.com>
2021-05-18 10:07:05 -0400

    Routing hacia login y crear cuenta

---
+++ pages/crearcuenta.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const CrearCuenta = () => {
+    return (
+        <Layout>
+            <p>Crear cuenta</p>
+        </Layout>
+     );
+}
+ 
+export default CrearCuenta;
