f3ce4136655a3999e9025ba7d2231c5814da8d84
Manuel <mninahuanca4@gmail.com>
2021-05-19 01:21:25 -0400

    Creando el cliente axios

--- context/auth/authReducer.js
+++ context/auth/authReducer.js
@@ -1,6 +1,6 @@
 import { USUARIO_AUTENTICADO } from "../../types";
 
-export default (state, action) => {
+const authReducer = (state, action) => {
     switch (action.type) {
         case USUARIO_AUTENTICADO:
             return {
@@ -11,3 +11,5 @@ export default (state, action) => {
             break;
     }
 }
+
+export default authReducer;
