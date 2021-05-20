5493f0c5213b45010afeeac472f7efd73d6251af
Manuel <mninahuanca4@gmail.com>
2021-05-20 15:57:19 -0400

    Creando un componente para el dropzone

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect } from 'react'
 import Layout from '../components/Layout';
 import authContext from '../context/auth/authContext';
 import Link from 'next/link';
+import Dropzone from '../components/Dropzone';
+
 
 export default function Home() {
 
@@ -17,9 +19,8 @@ export default function Home() {
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
         <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
-          <div className="md:flex-1 mb-3 mx-2 mt-12 ls:mt-0">
-            <p>Dropzone Aqui</p>
-          </div>
+          
+          <Dropzone />
 
           <div className="md:flex-1 mb-3 mx-2 mt-12 ls:mt-0">
             <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">
