b67f9af9a283d6027acd88798f81416e0a739e39
Manuel <mninahuanca4@gmail.com>
2021-05-20 15:46:44 -0400

    Primeros pasos con la pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,39 @@
-import React, { useContext,useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Layout from '../components/Layout';
 import authContext from '../context/auth/authContext';
+import Link from 'next/link';
 
 export default function Home() {
 
   // extraer el usuario autenticado del storage
   const AuthContext = useContext(authContext);
   const { usuarioAutenticado } = AuthContext;
-  
+
   useEffect(() => {
     usuarioAutenticado();
   }, [])
 
   return (
     <Layout>
-      <h1>index</h1>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+          <div className="md:flex-1 mb-3 mx-2 mt-12 ls:mt-0">
+            <p>Dropzone Aqui</p>
+          </div>
+
+          <div className="md:flex-1 mb-3 mx-2 mt-12 ls:mt-0">
+            <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">
+              Compartit archivos de forma sencilla y privada
+            </h2>
+            <p className="text-lg leading-loose">
+              <span className="text-red-500 font-bold">React Node Send</span> te permite compartir archivos con cifrado de extremo a extremo y un archivo que es eliminado despúes de ser descargado. Así que puedes mantener lo que compartes en privado y asegurarte de que tus cosas no permanezcan en línea para siempre.
+            </p>
+            <Link href="/crearcuenta">
+              <a className="text-red-500 font-bold text-lg hover:text-red-700">Crea una nueva cuenta</a>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
