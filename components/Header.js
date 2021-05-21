9a7bbf38d07d86962aea8df7172a528561bbb904
Manuel <mninahuanca4@gmail.com>
2021-05-21 12:15:56 -0400

    Routing dinamico con Next.js

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
         <header className="py-8 flex flex-col md:flex-row items.center justify-between">
 
             <Link href="/">
-                <img className="cursor-pointer w-64 mb-8 md:mb-0" src="logo.svg" />
+                <img className="cursor-pointer w-64 mb-8 md:mb-0" src="/logo.svg" />
             </Link>
 
             <div>
---
+++ pages/enlaces/[enlace].js
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout';
+
+export default () => {
+    return (
+        <Layout>
+            <p>desde enlace js</p>
+        </Layout>
+    );
+}
