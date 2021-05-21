ef13e480a64a5bf25de6385b68589616daa6b0ba
Manuel <mninahuanca4@gmail.com>
2021-05-21 12:39:15 -0400

    configurando las funciones para generar las URLs

--- pages/enlaces/[enlace].js
+++ pages/enlaces/[enlace].js
@@ -2,17 +2,31 @@ import Layout from '../../components/Layout';
 import clienteAxios from '../../config/axios';
 
 export async function getStaticProps() {
-    
+    const resultado = await clienteAxios.get('/api/enlaces/ReMggKwwY');
+    return {
+        props: {
+            enlace: resultado.data
+            
+        }
+    }
 }
 
 export async function getStaticPaths() {
-    
+    const enlaces = await clienteAxios.get('/api/enlaces');
+    return {
+        paths: enlaces.data.enlaces.map(enlace => ({
+            params: {enlace:enlace.url}
+        })),
+        fallback: false,
+    }
 }
 
-export default () => {
+const Enlace = ({ enlace }) => {
     return (
         <Layout>
             <p>desde enlace js</p>
         </Layout>
     );
 }
+
+export default Enlace;
