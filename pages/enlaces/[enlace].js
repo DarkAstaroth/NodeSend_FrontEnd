d9f366eeb2e973719dc9e569cd8e99bcf01395d6
Manuel <mninahuanca4@gmail.com>
2021-05-23 18:04:42 -0400

    Verificando si el enlace tiene password

--- pages/enlaces/[enlace].js
+++ pages/enlaces/[enlace].js
@@ -1,5 +1,6 @@
 import Layout from '../../components/Layout';
 import clienteAxios from '../../config/axios';
+import React, { useState } from 'react';
 
 export async function getServerSideProps({ params }) {
     const { enlace } = params;
@@ -16,7 +17,7 @@ export async function getServerSidePaths() {
     const enlaces = await clienteAxios.get('/api/enlaces');
     return {
         paths: enlaces.data.enlaces.map(enlace => ({
-            params: {enlace:enlace.url}
+            params: { enlace: enlace.url }
         })),
         fallback: false,
     }
@@ -24,15 +25,66 @@ export async function getServerSidePaths() {
 
 const Enlace = ({ enlace }) => {
 
+    const [tienePassword, setTienePassword] = useState(enlace.password);
+    console.log(tienePassword);
+
+    const verificarPassword = e => {
+        e.preventDefault();
+        console.log('verificando....');
+    }
+
     return (
         <Layout>
-            <h1 className="text-4l text-center text-gray-700"> Descarga tu archivo:</h1>
-            <div className="flex items-center justify-center mt-10">
-                <a
-                    href={`${process.env.backendURL}/api/archivos/${enlace.archivo}`}
-                    className="bg-red-500 text-center px-10 py-4 rounded uppercase font-bold text-white cursor-pointer">Aqui</a>
-            </div>
-        </Layout>
+            {
+                tienePassword ? (
+                    <>
+                        <p className="text-center">Este enlace esta protegido por un password, colocalo a continuacion</p>
+                        <div className="flex justify-center mt-5">
+                            <div className="w-full max-w-lg">
+                                <form
+                                    className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                                    onSubmit={ e=> verificarPassword(e) }
+                                >
+                                    <div className="mb-4">
+
+                                        <label
+                                            htmlFor="nombre"
+                                            className="block text-black text-sm font-bold mb-2">
+                                            Password
+                                    </label>
+
+                                        <input
+                                            className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            placeholder="Password del archivo"
+                                        />
+
+                                    </div>
+
+                                    <input
+                                        className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold"
+                                        type="submit"
+                                        value="Validar password..." />
+                                </form>
+                            </div>
+                        </div>
+                    </>
+                )
+                    : (
+                        <>
+                            <h1 className="text-4l text-center text-gray-700"> Descarga tu archivo:</h1>
+                            <div className="flex items-center justify-center mt-10">
+                                <a
+                                    href={`${process.env.backendURL}/api/archivos/${enlace.archivo}`}
+                                    className="bg-red-500 text-center px-10 py-4 rounded uppercase font-bold text-white cursor-pointer">Aqui</a>
+                            </div>
+                        </>
+                    )
+            }
+
+        </Layout >
     );
 }
 
